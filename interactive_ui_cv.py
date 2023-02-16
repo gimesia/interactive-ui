51c7c2afd9ae4bfcb39d955f282d811f603c0f71
Aron Gimesi <gimesiaron@gmail.com>
2023-02-16 16:03:34 +0100

    fix: error messages

--- interactive_ui_cv.py
+++ interactive_ui_cv.py
@@ -60,7 +60,7 @@ class ImageWindow():
         self.on_trackbar_changed = []
         self.edit = False
         self.preview = True
-        self.stats = False
+        self.stats = True
         if img is not None:
             self.set_base_image(img)
 
@@ -226,22 +226,26 @@ class ImageWindow():
         Returns:
             pandas.DataFrame: Dataframe with object properties
         """
+        # TODO: SOMETHING'S FUCKY, reproduce: trackbars to max
+
         df = pd.DataFrame({"cluster": [], "index": [], "area": []})
         for i, cluster in enumerate(self.clusters):
             if not cluster.checked:
                 continue
-            for j, contour in enumerate(self.clusters[i].contours):
-                # TODO: Enhance peformance-wise
-                df.loc[len(df.index)] = [
-                    cluster.name, j, cv.contourArea(contour)]
-            if disableds:
-                for j, contour in enumerate(self.clusters[i].disabled_contours):
+            if len(cluster.contours):
+                for j, contour in enumerate(self.clusters[i].contours):
                     # TODO: Enhance peformance-wise
                     df.loc[len(df.index)] = [
                         cluster.name, j, cv.contourArea(contour)]
+
+                if disableds:
+                    for j, contour in enumerate(self.clusters[i].disabled_contours):
+                        # TODO: Enhance peformance-wise
+                        df.loc[len(df.index)] = [
+                            cluster.name, j, cv.contourArea(contour)]
         return df
 
-    @debounce(1)
+    @debounce(0.5)
     def set_data(self):
         """Sets class property 'data' to current state of clusters
         """
@@ -256,9 +260,12 @@ class ImageWindow():
             list(map(lambda x: len(x.disabled_contours), self.clusters))))
         all = np.sum(lengths)
         indexes = list(map(lambda x: x.name, self.clusters))
-
-        frame = {'Count': pd.Series(lengths, index=indexes),
-                 'Percentage': pd.Series((lengths / all) * 100, index=indexes)}
+        if not all:
+            frame = {'Count': np.zeros(
+                len(lengths),), 'Percentage': np.zeros(len(lengths),)}
+        else:
+            frame = {'Count': pd.Series(lengths, index=indexes),
+                     'Percentage': pd.Series((lengths / all) * 100, index=indexes)}
         df1 = pd.DataFrame(frame, index=indexes)
         df2 = pd.DataFrame({'Count': [all, disableds], 'Percentage': [
             100, 0]}, index=['all', 'disabled'])
@@ -276,6 +283,8 @@ class ImageWindow():
             lines.append(
                 f"{index}: {int(row['Count'])} ({round(row['Percentage'], 2) if bool(row['Count']) else '0'}%)")
         image, textbox = put_textbox_on_img(image, lines, (25, 25))
+
+        # if textbox.shape[0] and textbox.shape[1]:
         cv.imshow("Statistics", textbox)
 
     def on_trackbar_sliders_changed(self):
@@ -424,6 +433,9 @@ def divide_contours_into_clusters(contours: np.ndarray, ) -> "list[Cluster]":
     # List of the areas of countours (matching indices)
     areas = list(map(lambda x: cv.contourArea(x), contours))
 
+    if not len(areas):
+        return clusters
+
     mn = np.floor(np.min(areas))
     mx = np.ceil(np.max(areas))
 
@@ -535,65 +547,14 @@ def put_textbox_on_img(img, lines: "list[str]", start_point: "tuple(int, int)", 
     return image, image[start_point[0]:end_point[0] - offset_y, start_point[1]:end_point[1]]
 
 
-def manual_on_img(img2):
-    shape = (400, 648, 3)
-    img = np.zeros(shape)
-
-    outline_col = (100, 100, 100)
-
-    arrow_len = 70
-    arrow_col = (0, 100, 255)
-    arrow_offset = 25
-
-    tb_h = 30
-    bb_h = shape[0] - 20
-    but_w = 26
-    but_mid = int(10.5 * but_w)
-
-    cv.line(img, (0, tb_h), (shape[1], tb_h), outline_col, 3)
-    for i in range(1, 12):
-        cv.line(img, (i * but_w, 0), (i * but_w, tb_h), outline_col, 3)
-    tb_h += 10
-    cv.arrowedLine(img, (but_mid, tb_h + arrow_len),
-                   (but_mid, tb_h), arrow_col, 3)
-    cv.putText(img, "Open 'controls' window", (but_mid - 50, tb_h + arrow_len + arrow_offset),
-               cv.FONT_HERSHEY_SIMPLEX, 1, arrow_col, 3)
-
-    arrow_col = (0, 255, 0)
-    cv.line(img, (0, bb_h), (shape[1], bb_h), outline_col, 3)
-    bb_h -= 10
-    cv.arrowedLine(img, (50, bb_h - arrow_len), (50, bb_h), arrow_col, 3)
-    cv.putText(img, "Selected object props", (5, bb_h - (arrow_len + (arrow_offset - 10))),
-               cv.FONT_HERSHEY_SIMPLEX, 1, arrow_col, 3)
-
-    pw = 5
-    img[:, 0:pw] = outline_col
-    img[:, -pw:] = outline_col
-    img[0:pw, :] = outline_col
-    img[-pw:, :] = outline_col
-    h, w = img.shape[0], img.shape[1]
-
-    # load background image as grayscale
-    back = img2.copy()
-    hh, ww = back.shape[0], back.shape[1]
-
-    # compute xoff and yoff for placement of upper left corner of resized image
-    yoff = round((hh - h) / 2)
-    xoff = round((ww - w) / 2)
-
-    # use numpy indexing to place the resized image in the center of background image
-    result = back.copy()
-    result[yoff:yoff + h, xoff:xoff + w] = img
-    return result
-
-
-def opencving():
-    """Test of the code
-    """
-    # NOTE: relative hardcoded path, might need to change
-    window = ImageWindow(cv.imread("img\cells8.tif"))
-    window.open_window()
+def centroid_for_contour(contour):
+    M = cv.moments(contour)
+    cx = int(M['m10'] / M['m00'])
+    cy = int(M['m01'] / M['m00'])
+    return (cx, cy)
 
 
 if __name__ == "__main__":
-    opencving()
+    # NOTE: relative hardcoded path, might need to change
+    window = ImageWindow(cv.imread("img\cells8.tif"))
+    window.open_window()
